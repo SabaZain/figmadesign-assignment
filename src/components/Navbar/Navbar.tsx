055144ab939a707ca6f1d40c0f8beb78ce51365a
Saba Zain <saba_punjwani@yahoo.com>
2024-11-05 16:02:16 +0500

    figma design assignment

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#A29875] w-full sm:w-[1920px] sm:h-auto'>
+      <h1 className='text-[#FFFFFF] text-5xl font-serif font-normal 
+      w-[439px] h-[94px] pt-6 pl-6'>MANZZARI</h1>
+    </div>
+  )
+}
+
+export default Navbar
